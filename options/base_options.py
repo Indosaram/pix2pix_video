ae6cc7cf80126c96b312708d3bbe6da7179ee0c7
jctestud <jean.christophe.testud@gmail.com>
2018-11-27 20:23:22 -0500

    add seed choice options

--- options/base_options.py
+++ options/base_options.py
@@ -34,6 +34,7 @@ class BaseOptions():
         self.parser.add_argument('--scheduled_sampling', action='store_true', default=False, help='activate scheduled sampling inspired training')
         self.parser.add_argument('--ss_recursion_prob', type=float, default=0.2, help='probability, at a given step, to use the previous step\'s generated frame as input image')
         self.parser.add_argument('--fps', type=float, default=24., help='frame per second for video generation')
+        self.parser.add_argument('--start_from', type=str, default="video", help='seed choice: "video" to use the first frames of the dataset, "noise" to start from a ramdom noise frame, or an image filepath')
         self.parser.add_argument('--zoom_lvl', default=0, type=int, help='amount of zoom during the feedback loop')
         self.parser.add_argument('--pstart', default=1, type=int, help='starting epoch for progress videos')
         self.parser.add_argument('--pstop', default=1, type=int, help='stopping epoch for progress videos')
