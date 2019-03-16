72c9426b2632c7992173abcad481f354afcef634
jctestud <jean.christophe.testud@gmail.com>
2019-03-16 18:59:44 -0400

    add a function to save images with text

--- video_utils.py
+++ video_utils.py
@@ -27,6 +27,21 @@ def save_tensor(tensor, path, text="", text_pos="auto", text_color=(255,255,255)
 
     img_pil.save(path)
 
+def save_img(img_pil, path, text="", text_pos="auto", text_color=(255,255,255)):
+    if text != "":
+        if text_pos == "auto":
+            # top-right corner
+            text_xpos = img_pil.width - 28 * len(text)
+            text_ypos = 30
+        else:
+            text_xpos, text_ypos = text_pos
+
+        draw = ImageDraw.Draw(img_pil)
+        font = ImageFont.truetype("/usr/share/fonts/truetype/ubuntu/Ubuntu-B.ttf", 50)
+        draw.text((text_xpos, text_ypos), text, text_color, font=font)
+
+    img_pil.save(path)
+
 
 def im2tensor(img_pil):
     """Go from a PIL image (0..255 RGB) to a (-1..1) tensor"""
@@ -67,19 +82,19 @@ def extract_frames_from_video(video_path, frame_dir, output_shape=(1280, 736), f
     p = subprocess.Popen(shlex.split(command), shell=False)
     p.communicate()
 
-def video_from_frame_directory(frame_dir, video_path, frame_file_glob=r"frame-%05d.jpg", framerate=24, ffmpeg_verbosity=16, crop_to_720p=True):
+def video_from_frame_directory(frame_dir, video_path, frame_file_glob=r"frame-%05d.jpg", framerate=24, ffmpeg_verbosity=16, crop_to_720p=True, reverse=False):
     """Build a mp4 video from a directory frames
         note: crop_to_720p crops the top of 1280x736 images to get them to 1280x720
     """
-    command = """ffmpeg -v %d -framerate %d -i %s -ss 1 -q:v 2%s %s""" % (
+    command = """ffmpeg -v %d -framerate %d -i %s -ss 1 -q:v 2%s %s%s""" % (
         ffmpeg_verbosity,
         framerate,
         frame_dir + "/" + frame_file_glob,
         ' -filter:v "crop=1280:720:0:16"' if crop_to_720p else "",
+        "-vf reverse " if reverse else "",
         video_path
     )
     print(command)
     print("building video from frames")
     p = subprocess.Popen(shlex.split(command), shell=False)
     p.communicate()
-
